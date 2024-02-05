a3f1a0401c558c431f308acec3650e217ae6162c
fred <fred>
2024-02-05 05:40:35 -0800

    second commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//var settingsmenu = document.querySelector(".settings-menu");
+
+
+
+//function settingsMenuToggle(){
+   // settingsmenu.classList.toggle("settings-menu-height");
+
+//}
+
+
+
+var settingMenu = document.querySelector(".setting-menu");
+ function settingsMenuToggle(){
+    settingsmenu.classlist.toggle("setting-menu");
+    
+ }
